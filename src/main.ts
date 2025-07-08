c5e75503c0279ca59d2e1435ca443dcc59930a4a
Elizaveta Veselova <veselovae1605@yandex.ru>
2025-07-08 19:39:59 +0300

    initialized Vue application and installed required dependencies

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { createPinia } from "pinia";
+import piniaPersist from "pinia-plugin-persistedstate";
+import ElementPlus from "element-plus";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+
+const pinia = createPinia();
+pinia.use(piniaPersist);
+
+const app = createApp(App);
+
+app.use(pinia);
+app.use(ElementPlus);
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component);
+}
+
+app.mount('#app')
